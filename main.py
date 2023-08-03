7ecdd08576e0e02b1ec354f17512cae90d927e54
Nakarin K <belizwp@live.com>
2023-08-03 14:22:24 +0700

    refac: containerize action

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import os
+import uuid
 from langchain.chat_models import ChatOpenAI
 
-MODEL = os.environ.get("MODEL", "gpt-3.5-turbo")
-TEMPERATURE = float(os.environ.get("TEMPERATURE", 0.7))
-PROMPT = os.environ.get("PROMPT", "")
+def set_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
 
-if PROMPT == "":
-    os._exit(0)
 
-chat_model = ChatOpenAI(
-    model=MODEL,
-    temperature=TEMPERATURE,
-)
-response = chat_model.predict(PROMPT)
+def set_multiline_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        delimiter = uuid.uuid1()
+        print(f'{name}<<{delimiter}', file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
 
-print(response)
+
+def main():
+    model = os.environ.get("MODEL", "gpt-3.5-turbo")
+    temperature = float(os.environ.get("TEMPERATURE", 0.7))
+    prompt = os.environ.get("PROMPT", "")
+
+    if prompt == "":
+        return
+
+    chat_model = ChatOpenAI(
+        model=model,
+        temperature=temperature,
+    )
+    response = chat_model.predict(prompt)
+
+    set_multiline_output("output", response)
+
+
+if __name__ == "__main__":
+    main()
